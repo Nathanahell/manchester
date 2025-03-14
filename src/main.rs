ddc669d833f1bc36b69199353be3cbcefeb4e25a
lisanalgaib <kingwannabe98@gmail.com>
2025-03-14 14:07:54 +0100

    Static include of cheatsheets fixed; the search is done in the cheetsheets based on the files in the binary

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-
 mod app;
-mod ui;
 mod art;
+mod ui;
 
 use app::{CheatSheet, CommandContext};
 use cli_log::*; // also import logging macros
@@ -9,56 +8,40 @@ use cli_log::*; // also import logging macros
 use std::collections::HashMap;
 use std::{error::Error, io};
 
-use std::{time, thread}; // For debug
+use std::{thread, time}; // For debug
 
 use ratatui::{
     crossterm::{
         event::{
-            self, 
-            DisableMouseCapture, 
-            EnableMouseCapture, 
-            Event, 
-            KeyEvent,
-            KeyCode ,
-            KeyEventKind,
-        }, 
-        execute, 
+            self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEvent, KeyEventKind,
+        },
+        execute,
         terminal::{
-            self, 
-            disable_raw_mode, 
-            enable_raw_mode, 
-            EnterAlternateScreen, 
-            LeaveAlternateScreen
+            self, disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
         },
-    }, 
-    widgets::{
-        TableState,
-        ScrollbarState
     },
-    prelude::{
-        Backend,
-         CrosstermBackend
-        },
-        Terminal};
+    prelude::{Backend, CrosstermBackend},
+    widgets::{ScrollbarState, TableState},
+    Terminal,
+};
 
 use std::path::Path;
 
-use include_dir::{include_dir, Dir, File};
 use glob::{glob, PatternError};
+use include_dir::{include_dir, Dir, File};
 
 use crate::{
-    app::{App, CurrentScreen, generate_test_data},
+    app::{generate_test_data, App, CurrentScreen},
     ui::ui,
 };
 
 static RESSOURCES_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/cheats");
 
-
 static easter_egg: &str = include_str!("easter_egg.txt");
 
-    /// Main function
-    // Prep & clean the terminal. Handle unexpected app exits and returns terminal to normal state
-fn main() -> Result<(), Box<dyn Error>>{
+/// Main function
+// Prep & clean the terminal. Handle unexpected app exits and returns terminal to normal state
+fn main() -> Result<(), Box<dyn Error>> {
     // === Logging
     init_cli_log!("MANCHESTER_APP");
 
@@ -105,12 +88,12 @@ fn main() -> Result<(), Box<dyn Error>>{
 
     // === Post run
     // restore terminal state back to normal
-    /*  
+    /*
     if app exists without running the following boilerplante, the terminal will act strangely
     the user will usually have to end the terminal session & start a new one
     It is important that we handle our error in such way tha we can call this last piece
-    We are printing the output or the errors after "execute!(LeaveAlternateScreen)" so that 
-    our prints will be rendered in the "new" scren and not lost in the alternate screen we just left 
+    We are printing the output or the errors after "execute!(LeaveAlternateScreen)" so that
+    our prints will be rendered in the "new" scren and not lost in the alternate screen we just left
     */
 
     disable_raw_mode()?;
@@ -133,9 +116,7 @@ fn main() -> Result<(), Box<dyn Error>>{
 
 /// Main loop that draw frames into the terminal
 /// Use generic type B implementing the Backend trait, to be backend agnostic
-pub fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<bool>{
-
-    
+pub fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<bool> {
     // === Main loop with events
     // Handles search, navigation + selection of search results
     loop {
@@ -145,7 +126,7 @@ pub fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Res
             dbg!(key.code);
         }
         */
-        
+
         // Draw a frame to the terminal, runs closure for each frame
         // Should we pass a mutable or immutable borrow of app here ?
         terminal.draw(|frame| ui(frame, app))?;
@@ -159,7 +140,7 @@ pub fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Res
 
             // Match key events depending of the screen
             match app.current_screen {
-                // For now, the app state should always be either CurrentlyEditing::Search or Variable 
+                // For now, the app state should always be either CurrentlyEditing::Search or Variable
                 CurrentScreen::Main => match key_event.code {
                     // Exit on 'Esc'
                     KeyCode::Esc => {
@@ -202,7 +183,7 @@ pub fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Res
                         app.update_after_search();
                     }
                     _ => {}
-                }
+                },
                 CurrentScreen::EditingCommand => {
                     match key_event.code {
                         // On 'Esc', go back to main screen, flush characters in editing string
@@ -216,7 +197,8 @@ pub fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Res
                         }
                         KeyCode::Char(char_value) => {
                             app.variable_value_input.push(char_value);
-                            todo!("
+                            todo!(
+                                "
                             In ui()
                             - Show variable value being inputed
                             - Dynamically completed the command"
@@ -224,7 +206,8 @@ pub fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Res
                         }
                         KeyCode::Backspace => {
                             app.variable_value_input.pop();
-                            todo!("
+                            todo!(
+                                "
                             In ui()
                             - Show variable value being inputed
                             - Dynamically completed the command"
@@ -240,31 +223,23 @@ pub fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Res
                         _ => {}
                     }
                 }
-            }        
+            }
         }
     }
 }
 
 // TODO : implement error handling for each unwrap + match arms
-pub fn read_cheatsheets() ->Vec<&'static include_dir::File<'static>> {
+pub fn read_cheatsheets() -> Vec<&'static include_dir::File<'static>> {
     // Define the glob pattern to match files with the .md extension
-    let glob_pattern = format!("{}/cheats/**/*.md", env!("CARGO_MANIFEST_DIR"));
+    // let glob_pattern = format!("{}/cheats/**/*.md", env!("CARGO_MANIFEST_DIR"));
+    let glob_pattern = "**/*.md";
     let mut files: Vec<&'static include_dir::File> = vec![];
 
-    for entry in glob(&glob_pattern).unwrap() {
-        match entry {
-            Ok(path) => {
-                // Strip prefix to match the included directory structure
-                let relative_path = path.strip_prefix(Path::new(env!("CARGO_MANIFEST_DIR")).join("cheats")).unwrap();
-                if let Some(file) = RESSOURCES_DIR.get_file(relative_path) {
-                    files.push(file);
-                    //println!("File path {}", file.path().display());
-                    //println!("{}", file.contents_utf8().unwrap());
-                }
-            }
-            Err(err) => {
-                eprintln!("{:?}", err);
-            }
+    for dirEntry in RESSOURCES_DIR.find(glob_pattern).unwrap() {
+        if let Some(file) = RESSOURCES_DIR.get_file(dirEntry.path()) {
+            files.push(file);
+            //println!("File path {}", file.path().display());
+            //println!("{}", file.contents_utf8().unwrap());
         }
     }
     files
@@ -272,10 +247,10 @@ pub fn read_cheatsheets() ->Vec<&'static include_dir::File<'static>> {
 
 pub fn parse_cheatsheets(files: Vec<&File<'static>>) -> Vec<CheatSheet> {
     // CheatSheet to populate
-    let mut cheatsheets : Vec<CheatSheet> = Vec::new();
+    let mut cheatsheets: Vec<CheatSheet> = Vec::new();
 
     // CommandContext fields to populate
-    let mut command_name= String::new();
+    let mut command_name = String::new();
     let mut tags: Vec<String> = Vec::new();
     let mut command = String::new();
     // Other fields are left blanks for now
@@ -286,9 +261,9 @@ pub fn parse_cheatsheets(files: Vec<&File<'static>>) -> Vec<CheatSheet> {
             None => {
                 println!("No content found for {}", file.path().display());
                 ""
-            },
+            }
         };
-        let lines:Vec<String> = contents.lines().map(String::from).collect();
+        let lines: Vec<String> = contents.lines().map(String::from).collect();
 
         // New cheatsheet to fill
         let mut cheatsheet = CheatSheet::default();
@@ -308,45 +283,50 @@ pub fn parse_cheatsheets(files: Vec<&File<'static>>) -> Vec<CheatSheet> {
             //dbg!("Cur line {}", &line);
 
             if cleaned_line.is_empty() {
-            } else if cleaned_line.starts_with("# ") { // Cheatsheet name
+            } else if cleaned_line.starts_with("# ") {
+                // Cheatsheet name
                 let cheatsheet_name = cleaned_line.replace("# ", "");
                 cheatsheet.name = cheatsheet_name;
-            } else if cleaned_line.starts_with("% ") { // Cheatsheet tags
+            } else if cleaned_line.starts_with("% ") {
+                // Cheatsheet tags
                 let mut cheatsheet_tags: Vec<String> = cleaned_line
-                .replace("% ", "")
-                .split(',')
-                .map(|e| { e.to_string() })
-                .collect();
-                cheatsheet_tags = cheatsheet_tags.into_iter().map(|e| { e.to_string() }).collect();
+                    .replace("% ", "")
+                    .split(',')
+                    .map(|e| e.to_string())
+                    .collect();
+                cheatsheet_tags = cheatsheet_tags.into_iter().map(|e| e.to_string()).collect();
                 cheatsheet.sheet_tag = cheatsheet_tags;
             } else if cleaned_line.starts_with("## ") {
                 command_name = cleaned_line.replace("## ", "");
-            } else if cleaned_line.starts_with("#") { // Command tags
+            } else if cleaned_line.starts_with("#") {
+                // Command tags
                 let tags_beforematch: Vec<String> = cleaned_line
-                .replace('#', "")
-                .split(' ')
-                .map(|e| { e.to_string() })
-                .collect();
+                    .replace('#', "")
+                    .split(' ')
+                    .map(|e| e.to_string())
+                    .collect();
                 //tags = tags.into_iter().map(|e| {e.to_string() }).collect();
-                
+
                 // More compact tags representation, if a matching exists
                 for tag in tags_beforematch {
                     match tags_mapping.get(&tag) {
                         Some(tag) => tags.push(tag.to_string()),
-                        None => tags.push(tag.to_string())
+                        None => tags.push(tag.to_string()),
                     }
                 }
                 // dbg!(&tags);
-            } else if cleaned_line.contains("```") && !is_parsing_cmd{ // Start command parsing
+            } else if cleaned_line.contains("```") && !is_parsing_cmd {
+                // Start command parsing
                 is_parsing_cmd = true;
-            } else if cleaned_line.contains("```") && is_parsing_cmd { // Stop command parsing
+            } else if cleaned_line.contains("```") && is_parsing_cmd {
+                // Stop command parsing
                 is_parsing_cmd = false;
                 multiline_cmd = false;
                 //dbg!(&command);
                 //dbg!("Command parsed");
 
                 commands.push({
-                    CommandContext{
+                    CommandContext {
                         command_name,
                         tags,
                         command,
@@ -357,7 +337,8 @@ pub fn parse_cheatsheets(files: Vec<&File<'static>>) -> Vec<CheatSheet> {
                 command_name = String::new();
                 tags = Vec::new();
                 command = String::new();
-            } else if is_parsing_cmd { // command
+            } else if is_parsing_cmd {
+                // command
                 // TODO : Argument prefill / Dynamic argument completion
 
                 // if the other lines are a command, is evaluated to true
@@ -377,8 +358,8 @@ pub fn parse_cheatsheets(files: Vec<&File<'static>>) -> Vec<CheatSheet> {
 }
 
 pub fn generate_tags_mapping() -> Result<HashMap<String, String>, serde_json::Error> {
-        // JSON text    
-        let tags_dict = r#"
+    // JSON text
+    let tags_dict = r#"
             {"target/local": "Loc",
             "target/remote": "Rem",
             "target/serve": "Ser",
@@ -387,11 +368,10 @@ pub fn generate_tags_mapping() -> Result<HashMap<String, String>, serde_json::Er
             "plateform/mac": "[M] ",
             "plateform/multiple": "[*] "}
         "#;
-        // Deserialize the JSON text into a HashMap
-        let mapping: HashMap<String, String> = serde_json::from_str(tags_dict)?;
-        Ok(mapping)
+    // Deserialize the JSON text into a HashMap
+    let mapping: HashMap<String, String> = serde_json::from_str(tags_dict)?;
+    Ok(mapping)
 }
-
 
 #[cfg(test)]
 mod test {
@@ -402,7 +382,7 @@ mod test {
 
     use super::*;
     #[test]
-    fn test_dry_run() -> Result<(), Box<dyn Error>>{
+    fn test_dry_run() -> Result<(), Box<dyn Error>> {
         // === Logging
         init_cli_log!("MANCHESTER_APP");
 
@@ -465,12 +445,12 @@ mod test {
                 candidates.push(&command.command);
             }
         }
-    
+
         let mut results = candidates
             .iter()
             .filter(|&candidate| matcher.fuzzy_match(candidate, query).is_some())
             .collect::<Vec<_>>();
-    
+
         results.sort_by_key(|&candidate| matcher.fuzzy_indices(candidate, query).unwrap().0);
         results.reverse();
         println!("Fuzzy search results for '{}':", query);
@@ -487,7 +467,7 @@ mod test {
         let files = read_cheatsheets();
         let cheatsheets = parse_cheatsheets(files);
         let mut commands = Vec::new();
-        
+
         for cheatsheet in cheatsheets {
             for command in cheatsheet.commands {
                 commands.push(command);
@@ -507,10 +487,10 @@ mod test {
             .iter()
             .filter(|&candidate| matcher.fuzzy_match(candidate, query).is_some())
             .collect::<Vec<_>>();
-    
+
         results.sort_by_key(|&candidate| matcher.fuzzy_indices(candidate, query).unwrap().0);
         results.reverse();
-        let string_results: Vec<String>  = results
+        let string_results: Vec<String> = results
             .iter()
             .map(|str_slice| str_slice.to_string())
             .collect();
@@ -522,7 +502,7 @@ mod test {
         }
          */
         let mut commands_after_search = Vec::new();
-        
+
         for result in &string_results {
             for commandcontext in &app.commands {
                 if commandcontext.command.as_str() == result {
