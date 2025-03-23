c2603b7b56ed44505a56ee79a0888357570e971f
lisanalgaib <kingwannabe98@gmail.com>
2025-03-23 11:34:25 +0100

    Cleared comments in app.rs

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,14 @@
-use std::{collections::HashMap, fmt::Debug, io::{Error, Result}};
-use ratatui::{self, widgets::{ScrollbarState, TableState, Widget}};
+use ratatui::{
+    self,
+    widgets::{ScrollbarState, TableState, Widget},
+};
+use std::{
+    collections::HashMap,
+    fmt::Debug,
+    io::{Error, Result},
+};
 
 use fuzzy_matcher::{skim::SkimMatcherV2, FuzzyMatcher};
-
 
 // Wrapper to implement Debug trait
 pub struct SkimMatcherV2Wrapper {
@@ -12,20 +18,22 @@ pub struct SkimMatcherV2Wrapper {
 impl Debug for SkimMatcherV2Wrapper {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("SkimMatcherV2Wrapper")
-        .field("matcher", &"SkimMatcherV2") // Placeholder for now
-        .finish()
+            .field("matcher", &"SkimMatcherV2") // Placeholder for now
+            .finish()
     }
 }
 
 impl SkimMatcherV2Wrapper {
     pub fn new() -> Self {
-        Self { matcher: SkimMatcherV2::default() }
-    }    
+        Self {
+            matcher: SkimMatcherV2::default(),
+        }
+    }
 }
 
 #[derive(Debug, PartialEq)]
 pub enum CurrentScreen {
-    Main, // Main screen + search
+    Main,           // Main screen + search
     EditingCommand, // TODO
 }
 
@@ -34,14 +42,13 @@ type Command = String;
 
 #[derive(Debug, Clone, Default)]
 pub struct CommandContext {
-    pub command_name : String,
+    pub command_name: String,
     pub tags: Vec<String>,
     pub command: Command,
-    pub variables_to_fill: Vec<String>, // UNEEDED ? : todo
-    pub variable_prefil_values:Vec<String> // UNEEDED ?
-    // field for variables with prefilled values ?
+    pub variables_to_fill: Vec<String>, // TODO
+    pub variable_prefil_values: Vec<String>, // TODO
+                                        // field for variables with prefilled values ?
 }
-
 
 // Not used
 // just for easier data visualization and if there is further extension of the app
@@ -49,15 +56,11 @@ pub struct CommandContext {
 pub struct CheatSheet {
     pub name: String,
     pub sheet_tag: Vec<String>,
-    pub commands: Vec<CommandContext>,    
+    pub commands: Vec<CommandContext>,
 }
 
 impl CheatSheet {
-    pub fn new(
-        name: String,
-        sheet_tag: Vec<String>,
-        commands: Vec<CommandContext>
-    ) -> Self {
+    pub fn new(name: String, sheet_tag: Vec<String>, commands: Vec<CommandContext>) -> Self {
         Self {
             name,
             sheet_tag,
@@ -71,14 +74,12 @@ pub struct App {
     pub search_value_input: String, // String to search within command names
     pub variable_value_input: String, // Input value to pass to editable variables
     pub commands: Vec<CommandContext>, // Exhaustive list of all commands
-    pub commands_after_search: Vec<CommandContext>, // List of commands matching the search. 
+    pub commands_after_search: Vec<CommandContext>, // List of commands matching the search.
     // FOR NOW : a clone of CommandContext are stored in the fields. For better performance, the search feature must be rewritten
-    // Using indexes of the vector field 'commands' to avoid self-referencn structure. 
-    // Interesting topic about self-ref struct : https://stackoverflow.com/questions/32300132/why-cant-i-store-a-value-and-a-reference-to-that-value-in-the-same-struct
+    // Using indexes of the vector field 'commands' to avoid self-referencn structure.
     pub output_command: String, // Completed command to input, the string should be prefilled wit the selected command after a search
     pub current_screen: CurrentScreen,
-    // pub search_index: usize, // USELESS since search_table_state handle it ? Index for search result element, to highlight current search result
-    pub search_table_state: TableState,
+    pub search_table_state: TableState, // search_table_state handles the selection. An index  is used to search result element, to highlight current search result
     pub editcommand_table_state: TableState,
     matcherwrapper: SkimMatcherV2Wrapper,
 }
@@ -94,7 +95,7 @@ impl App {
             current_screen: CurrentScreen::Main,
             search_table_state: TableState::new(),
             editcommand_table_state: TableState::new(),
-            matcherwrapper: SkimMatcherV2Wrapper::new(), 
+            matcherwrapper: SkimMatcherV2Wrapper::new(),
         }
     }
 
@@ -119,13 +120,13 @@ impl App {
 
     pub fn next_row(&mut self) {
         let index = match self.search_table_state.selected() {
-            Some(previous_selected_index)  => {
+            Some(previous_selected_index) => {
                 if previous_selected_index == 0 {
-                    self.commands_after_search.len() -1
+                    self.commands_after_search.len() - 1
                 } else {
                     previous_selected_index - 1
                 }
-            },
+            }
             None => 0,
         };
         self.search_table_state.select(Some(index));
@@ -143,74 +144,66 @@ impl App {
         let query = self.search_value_input.as_str();
 
         let mut results = candidates
-        .iter()
-        .filter(|&candidate| matcher.fuzzy_match(candidate, query).is_some())
-        .collect::<Vec<_>>();
+            .iter()
+            .filter(|&candidate| matcher.fuzzy_match(candidate, query).is_some())
+            .collect::<Vec<_>>();
 
         results.sort_by_key(|&candidate| matcher.fuzzy_indices(candidate, query).unwrap().0);
         results.reverse();
-        let string_results: Vec<String>  = results
+        let string_results: Vec<String> = results
             .iter()
             .map(|str_slice| str_slice.to_string())
             .collect();
 
-            let mut new_commands_after_search = Vec::new();
-            for result in &string_results {
-                for commandcontext in &self.commands {
-                    if commandcontext.command.as_str() == result {
-                        new_commands_after_search.push(commandcontext.clone());
-                    }
+        let mut new_commands_after_search = Vec::new();
+        for result in &string_results {
+            for commandcontext in &self.commands {
+                if commandcontext.command.as_str() == result {
+                    new_commands_after_search.push(commandcontext.clone());
                 }
             }
-            self.commands_after_search = new_commands_after_search;
+        }
+        self.commands_after_search = new_commands_after_search;
     }
 }
 
 pub fn generate_test_data() -> Vec<CommandContext> {
     let test_data = vec![
-        CommandContext { 
-            command_name: "Edge case - First command".to_string(), 
+        CommandContext {
+            command_name: "Edge case - First command".to_string(),
             tags: vec!["tag1".to_string(), "tag2".to_string(), "tag3".to_string()],
             command: "nmap -p- <IP>".to_string(),
             variables_to_fill: vec!["IP".to_string()],
-            variable_prefil_values: {Vec::new()}
+            variable_prefil_values: { Vec::new() },
         },
-        CommandContext { 
-            command_name: "nmap - UDP Full port scan".to_string(), 
+        CommandContext {
+            command_name: "nmap - UDP Full port scan".to_string(),
             tags: vec!["tag1".to_string(), "tag2".to_string(), "tag3".to_string()],
             command: "nmap -sU <IP>".to_string(),
             variables_to_fill: vec!["IP".to_string()],
-            variable_prefil_values: {
-                Vec::new()
-            }
+            variable_prefil_values: { Vec::new() },
         },
-        CommandContext { 
-            command_name: "dirbuster ".to_string(), 
+        CommandContext {
+            command_name: "dirbuster ".to_string(),
             tags: vec!["tag1".to_string(), "tag2".to_string(), "tag3".to_string()],
             command: "dirbuster -u http://<IP>:<PORT>".to_string(),
             variables_to_fill: vec!["IP".to_string()],
-            variable_prefil_values: {
-                Vec::new()
-            }
+            variable_prefil_values: { Vec::new() },
         },
-        CommandContext { 
-            command_name: "nmap - TCP Full port scan".to_string(), 
+        CommandContext {
+            command_name: "nmap - TCP Full port scan".to_string(),
             tags: vec!["tag1".to_string(), "tag2".to_string(), "tag3".to_string()],
             command: "nmap -p- <IP>".to_string(),
             variables_to_fill: vec!["IP".to_string()],
-            variable_prefil_values: {
-                Vec::new()
-            }
+            variable_prefil_values: { Vec::new() },
         },
-        CommandContext { 
-            command_name: "Edge case - Last command ".to_string(), 
+        CommandContext {
+            command_name: "Edge case - Last command ".to_string(),
             tags: vec!["tag1".to_string(), "tag2".to_string(), "tag3".to_string()],
             command: "dirbuster -u http://<IP>:<PORT>".to_string(),
             variables_to_fill: vec!["IP".to_string()],
-            variable_prefil_values: {
-                Vec::new()
-            }
-        }, 
+            variable_prefil_values: { Vec::new() },
+        },
     ];
     test_data
 }
